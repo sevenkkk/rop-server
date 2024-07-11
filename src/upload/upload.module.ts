6ef8bf05ea5c0610beb3b4cdf0a723aa7f4c3031
rat <ratvip666@gmail.com>
2024-07-11 14:25:03 +0800

    上传接口

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
-import { MulterModule } from '@nestjs/platform-express';
-import { diskStorage } from 'multer';
-import { mkdirsSync } from 'fs-extra';
 import { ShareModule } from '@/src/share/share.module';
 
 @Module({
-  imports: [
-    // MulterModule.register({
-    //   storage: diskStorage({
-    //     destination: (req, file, cb) => {
-    //       console.log(req.body);
-    //       const filePath = `public/${req.body}/${req.body}/${req.params.version}`;
-    //       mkdirsSync(filePath);
-    //       return cb(null, `./${filePath}`);
-    //     },
-    //     filename: (req, file, cb) => {
-    //       return cb(null, file.originalname);
-    //     },
-    //   }),
-    // }),
-    ShareModule,
-  ],
+  imports: [ShareModule],
   providers: [UploadService],
   controllers: [UploadController],
 })
